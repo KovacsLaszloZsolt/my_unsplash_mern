d01ac195dfd0494aa74342ec8103d493d03149c0
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-04-20 14:53:53 +0100

    fix: static route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use(errorHandler);
 // serve static folder if production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('../frontend/build'));
+  app.use('/', express.static('../frontend/build'));
   app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'build', 'index.html'));
   });

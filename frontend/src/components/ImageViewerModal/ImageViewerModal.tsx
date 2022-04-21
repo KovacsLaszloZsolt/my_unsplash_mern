d00f368eb630f1b0d4ae9bd92365b6e5dd1b307c
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-04-21 18:35:58 +0100

    feat: add ImageViewerModal

---
+++ frontend/src/components/ImageViewerModal/ImageViewerModal.tsx
@@ -0,0 +1,70 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import './ImageViewerModal.scss';
+import AppContext from '../../context/AppContext';
+import { Image } from '../../interfaces';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { ImArrowLeft, ImArrowRight } from 'react-icons/im';
+
+const ImageViewerModal = ({
+  index,
+  setIndex,
+}: {
+  index: number | null;
+  setIndex: React.Dispatch<React.SetStateAction<number | null>>;
+}): JSX.Element => {
+  const { data, getAllImages, setIsModalOpen } = useContext(AppContext);
+  const [image, setImage] = useState<Image | null>(index !== null ? data[index] : null);
+
+  const handleArrowClick = (direction: string): void => {
+    if (index !== null) {
+      if (direction === 'right') {
+        setIndex(index + 1);
+        return;
+      }
+      setIndex(index - 1);
+    }
+  };
+
+  const handleCloseClick = (): void => {
+    setImage(null);
+    setIndex(null);
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (index === data.length - 1) {
+      void getAllImages(data.length);
+    }
+  }, [index]);
+
+  useEffect(() => {
+    if (index !== null) {
+      setImage(data[index]);
+    }
+  }, [index]);
+  return (
+    <>
+      {image && (
+        <div className="modal">
+          <div className="imageFullCtn">
+            <AiFillCloseCircle className="closeIcon" onClick={handleCloseClick} />
+            {!!index && (
+              <div className="arrowCtn arrowLeft" onClick={() => handleArrowClick('left')}>
+                <ImArrowLeft />
+              </div>
+            )}
+            <img className="imageFull" src={image.url} alt={image.label} />
+            {!(index !== null && index + 1 === data.length) && (
+              <div className="arrowCtn arrowRight" onClick={() => handleArrowClick('right')}>
+                <ImArrowRight />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ImageViewerModal;
